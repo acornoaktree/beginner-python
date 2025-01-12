a945d0923fd8cd4360fd25261ddf2de29d7528a7
Darragh Cullinan <darragh.b.cullinan@gmail.com>
2025-01-12 14:47:35 +0000

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def home_page_view(request):
-    return HttpResponse("Hello, Pamela")
+    return HttpResponse("Hello, World!")
 
-# Create your views here.
+
