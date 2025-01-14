f2af610f106e7a029f5c87b50c2ddde9417dddb4
Darragh Cullinan <darragh.b.cullinan@gmail.com>
2025-01-14 19:29:21 +0000

    pg 82

--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page_view(request):
-    return HttpResponse("Homepage")
-
-def about_page_view(request):
-    context = {
-        "name": "Darragh",
-        "age": 16,
-        }
-    return render(request, "pages/about.html", context)
+    return render(request, "home.html")
