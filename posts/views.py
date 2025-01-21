a0bd001b40117b10dea8729de7d11d05d8738f7d
Darragh Cullinan <darragh.b.cullinan@gmail.com>
2025-01-21 19:42:20 +0000

    pg 118

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from .models import Post
+#from django.shortcuts import render
+#from .models import Post
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "post_list.html", {"posts": posts})
+#def post_list(request):
+#    posts = Post.objects.all()
+#    return render(request, "post_list.html", {"posts": posts})
 
 # Create your views here.
+
+from django.views.generic import ListView # new
+from .models import Post
+
+class PostList(ListView): # new
+    model = Post
+    template_name = "post_list.html"
