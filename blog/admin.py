aadbd4e0bbc8eae0068fbf8d4f725ae51a6f1ea5
Darragh Cullinan <darragh.b.cullinan@gmail.com>
2025-02-03 13:13:50 +0000

    pg 155

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-class PostAdmin(admin.ModelAdmin): # new 
+class PostAdmin(admin.ModelAdmin): 
     list_display = (
         "title",
         "author",
