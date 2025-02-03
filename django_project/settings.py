c5e72b1a8e081c73e1ef1767d006e0c0c2654b0b
Darragh Cullinan <darragh.b.cullinan@gmail.com>
2025-02-03 11:31:42 +0000

    pg 147

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pages",
-    "posts",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("pages", include("pages.urls")),
-    path("", include("posts.urls")),
+    path("", include("blog.urls"))
 ]
